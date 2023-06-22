87ce60f4698534085186f4cd50d7f233ffcb1952
Joshua Faessler <jffaessler@gmail.com>
2023-06-22 15:20:47 +0200

    Okay so something's wrong - I get the exact same velocity loss for the simulation

--- milestones/04/04.cpp
+++ milestones/04/04.cpp
@@ -4,23 +4,27 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    double timestep = 0.010;
-    int steps = 100;
+    double timestep = 0.00001;
+    int steps = static_cast<int>(1. / timestep / 10.);
 
     std::ofstream traj("traj.xyz");
     std::ofstream ts(std::to_string(timestep) + ".timestep");
 
-    auto [names, positions, velocities]{read_xyz_with_velocities("lj54.xyz")};
-    Atoms atoms(positions, velocities);
+    auto [names, init_positions,
+          init_velocities]{read_xyz_with_velocities("lj54.xyz")};
+    Atoms atoms(init_positions, init_velocities);
 
     write_xyz(traj, atoms);
-    ts << 0 * timestep << ", " << pow(atoms.velocities.sum(), 2) / 2 << std::endl; // mass = 1
+    ts << 0 * timestep << ", " << pow(atoms.velocities.sum(), 2) / 2
+       << std::endl;
     for (int i = 0; i < steps; ++i) {
         verlet_step1(atoms.positions, atoms.velocities, atoms.forces, timestep);
-        lj_direct_summation(atoms);
+        double pot = lj_direct_summation(atoms) / -1; // epsilon = 1
         verlet_step2(atoms.velocities, atoms.forces, timestep);
         write_xyz(traj, atoms);
-        ts << (i + 1) * timestep << "," << pow(atoms.velocities.sum(), 2) / 2 << std::endl; // mass = 1
+        auto kinetic = Eigen::pow(atoms.velocities, 2).sum() / 2;
+        ts << (i + 1) * timestep << ","
+           << pot + kinetic << std::endl;
     }
 
     traj.close();
