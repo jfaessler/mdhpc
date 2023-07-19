13aa3c29a37ecc7d33e8cd798a75f40b86e8f3e7
Joshua Faessler <jffaessler@gmail.com>
2023-07-19 11:50:14 +0200

    still trying to equilibriate

--- milestones/07/07.cpp
+++ milestones/07/07.cpp
@@ -8,10 +8,10 @@
 
 
 int main(int argc, char *argv[]) {
-    constexpr double timestep = 5.0;
-    constexpr int steps = 2000;
+    constexpr double timestep = 1.0;
+    constexpr int steps = 8000;
 
-    constexpr int snapshot_interval = steps / 1000; // 100 total frames
+    constexpr int snapshot_interval = steps / 100; // 100 total frames
     // TODO set according to time accumulated
 
     std::ofstream traj("traj.xyz");
@@ -22,12 +22,14 @@ int main(int argc, char *argv[]) {
     atoms.mass = 20413.15887; // Mass system's mass units where g/mol = 0.009649
     atoms.k_b = 8.617333262e-5; // Boltzmann constant in eV/K
 
-    const double cutoff = 10.0; // Default cutoff from ducastelle
+    const double cutoff = 5.0; // Default cutoff from ducastelle
     NeighborList neighborList;
     neighborList.update(atoms, cutoff);
 
-    double target_temp = 0.0;
-    const double relaxation = 100;
+    double target_temp = 100.0;
+    const double relaxation = 1000;
+
+    const int s = 800; // Scale for setting new temp value
 
     std::cout << "Step,Time,Total Energy,Potential,Kinetic,Temperature" << std::endl;
     std::cout.precision(10);
@@ -37,33 +39,38 @@ int main(int argc, char *argv[]) {
         neighborList.update(atoms, cutoff);
         double pot = ducastelle(atoms, neighborList, cutoff);
         verlet_step2(atoms.velocities, atoms.forces, timestep, atoms.mass);
-        berendsen_thermostat(atoms, target_temp, timestep, relaxation);
+        if ((i / s) % 2 == 0)
+            berendsen_thermostat(atoms, target_temp, timestep, relaxation);
 
-        if (i == 100) {
+        if (i == s) {
             target_temp = 500;
         }
-        if (i == 300) {
+        if (i == 3 * s) {
             target_temp = 600;
         }
 
-        if (i == 500) {
+        if (i == 5 * s) {
             target_temp = 650;
         }
 
-        if (i == 700) {
+        if (i == 7 * s) {
             target_temp = 700;
         }
 
-        if (i == 900) {
+        if (i == 9 * s) {
             target_temp = 725;
         }
 
-        if (i == 1100) {
+        if (i == 11 * s) {
             target_temp = 750;
         }
 
-        if (i == 1200) {
+        if (i == 12 * s) {
             target_temp = 800;
+        }
+
+        if (i == 14 * s) {
+            target_temp = 900;
         }
         if (i % snapshot_interval == 0) {
             write_xyz(traj, atoms);
