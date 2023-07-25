4e9eb48354d71893fa1528e04d3d3c02b4885ad5
Joshua Faessler <jffaessler@gmail.com>
2023-07-25 16:35:46 +0200

    Yep domain is what I thought it was supposed to be

--- milestones/08/08.cpp
+++ milestones/08/08.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
         20413.15887; // Gold in system's mass units where g/mol = 0.009649
     Atoms atoms(init_positions, gold_mass);
     atoms.k_b = 8.617333262e-5; // Boltzmann constant in eV/K
-    Domain domain(MPI_COMM_WORLD, {15, 15, 30}, {2, 2, 1}, {0, 0, 0});
+    Domain domain(MPI_COMM_WORLD, {30, 30, 30}, {2, 2, 1}, {0, 0, 0});
     domain.enable(atoms);
     domain.exchange_atoms(atoms);
     domain.update_ghosts(atoms, 2 * cutoff);
