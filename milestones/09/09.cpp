2be0ac4f704e349fab2e14ab48dcf29e72e03722
Joshua Faessler <jffaessler@gmail.com>
2023-08-02 15:55:48 +0200

    I may have fixed the error where it was finding a neighbor out of bounds since verlet2 moves atoms

--- milestones/09/09.cpp
+++ milestones/09/09.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
         20413.15887; // Gold in system's mass units where g/mol = 0.009649
     Atoms atoms(init_positions, gold_mass);
     atoms.k_b = 8.617333262e-5; // Boltzmann constant in eV/K
-    Domain domain(MPI_COMM_WORLD, {40.39, 40.8, 144.24978336}, {1, 1, 4},
+    Domain domain(MPI_COMM_WORLD, {40.39, 40.8, 144.24978336}, {1, 1, 1},
                   {0, 0, 1});
     domain.enable(atoms);
     domain.exchange_atoms(atoms);
@@ -56,14 +56,20 @@ int main(int argc, char *argv[]) {
         domain.exchange_atoms(atoms);
         domain.update_ghosts(atoms, 2 * cutoff);
         neighborList.update(atoms, cutoff);
-        ducastelle(atoms, neighborList, cutoff);
+        double pot = ducastelle(atoms, neighborList, cutoff);
         verlet_step2(atoms.velocities, atoms.forces, timestep, atoms.mass);
         if (i < eq_steps)
             berendsen_thermostat(atoms, eq_temp, timestep, eq_relax);
         else
             domain.scale(atoms, {len[0], len[1], len[2] + strain_rate});
 
+//        if (domain.rank() == 0) {
+//            std::cout << "Step:" << i << std::endl;
+//        }
         if (i % snapshot_interval == 0) {
+            domain.exchange_atoms(atoms);
+            domain.update_ghosts(atoms, 2 * cutoff);
+            neighborList.update(atoms, cutoff);
             Eigen::Matrix3d local_stress = stress_parallel(atoms, neighborList, domain, cutoff);
             Eigen::Matrix3Xd recv(3, domain.size() * 3);
             Eigen::ArrayXi recvcount(3 * domain.size());
@@ -72,9 +78,9 @@ int main(int argc, char *argv[]) {
             for (int d = 0; d < domain.size(); d++) {
                 displ[d] = 9 * d;
             }
-//            MPI_Allgatherv(local_stress.data(), 9, MPI_DOUBLE, recv.data(), recvcount.data(), displ.data(), MPI_DOUBLE, MPI_COMM_WORLD);
+            MPI_Allgatherv(local_stress.data(), 9, MPI_DOUBLE, recv.data(), recvcount.data(), displ.data(), MPI_DOUBLE, MPI_COMM_WORLD);
             if (domain.rank() == 0) {
-                //                std::cout << recv << std::endl;
+//                                std::cout << recv << std::endl;
                 Eigen::Matrix3d stress;
                 stress.setZero();
                 for (int d = 0; d < domain.size(); d++) {
@@ -90,8 +96,8 @@ int main(int argc, char *argv[]) {
                 write_xyz(*traj, atoms);
                 // Slower cheaty way of only measuring potentials of non-ghost
                 // atoms for write operations
-                neighborList.update(atoms, cutoff);
-                double pot = ducastelle(atoms, neighborList, cutoff);
+//                neighborList.update(atoms, cutoff);
+//                double pot = ducastelle(atoms, neighborList, cutoff);
                 auto kinetic = kinetic_energy(atoms);
                 std::cout << i << "," << (i)*timestep << "," << pot + kinetic
                           << "," << pot << "," << kinetic << ","
