880dcc2f66e300e05195b581b92528c7f0a6b7ba
Joshua Faessler <jffaessler@gmail.com>
2023-08-02 18:13:44 +0200

    I dont' even know

--- milestones/09/09.cpp
+++ milestones/09/09.cpp
@@ -1,3 +1,4 @@
+#include "analysis.h"
 #include "domain.h"
 #include "ducastelle.h"
 #include "mpi.h"
@@ -6,7 +7,6 @@
 #include "verlet.h"
 #include "xyz.h"
 #include <iostream>
-#include "analysis.h"
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
@@ -18,9 +18,9 @@ int main(int argc, char *argv[]) {
     constexpr int snapshot_interval = steps / 100; // 100 total frames
     constexpr double cutoff = 5.0;
     constexpr int eq_steps = 4000;
-    constexpr double eq_temp = 0.01;
-    constexpr double eq_relax = 1000.0;
-    constexpr double strain_rate = 0.001; // Strain per frame
+    constexpr double eq_temp = 100.0;
+    constexpr double eq_relax = 10.0;
+    constexpr double strain_rate = 0.000; // Strain per frame
     double original_length;
 
     NeighborList neighborList;
@@ -42,9 +42,9 @@ int main(int argc, char *argv[]) {
     std::ofstream *traj = nullptr;
     if (rank == 0) {
         traj = new std::ofstream("traj.xyz");
-        std::cout
-            << "Step,Time,Total Energy,Potential,Kinetic,Temperature,Strain,Stress"
-            << std::endl;
+        std::cout << "Step,Time,Total "
+                     "Energy,Potential,Kinetic,Temperature,Strain,Stress"
+                  << std::endl;
         std::cout.precision(10);
     }
 
@@ -58,19 +58,21 @@ int main(int argc, char *argv[]) {
         neighborList.update(atoms, cutoff);
         double pot = ducastelle(atoms, neighborList, cutoff);
         verlet_step2(atoms.velocities, atoms.forces, timestep, atoms.mass);
-        if (i < eq_steps)
+        if (i < eq_steps) {
             berendsen_thermostat(atoms, eq_temp, timestep, eq_relax);
-        else
+        } else {
             domain.scale(atoms, {len[0], len[1], len[2] + strain_rate});
+        }
 
-//        if (domain.rank() == 0) {
-//            std::cout << "Step:" << i << std::endl;
-//        }
+        //        if (domain.rank() == 0) {
+        //            std::cout << "Step:" << i << std::endl;
+        //        }
         if (i % snapshot_interval == 0) {
             domain.exchange_atoms(atoms);
             domain.update_ghosts(atoms, 2 * cutoff);
             neighborList.update(atoms, cutoff);
-            Eigen::Matrix3d local_stress = stress_parallel(atoms, neighborList, domain, cutoff);
+            Eigen::Matrix3d local_stress =
+                stress_parallel(atoms, neighborList, domain, cutoff);
             Eigen::Matrix3Xd recv(3, domain.size() * 3);
             Eigen::ArrayXi recvcount(3 * domain.size());
             recvcount.setConstant(9);
@@ -78,32 +80,37 @@ int main(int argc, char *argv[]) {
             for (int d = 0; d < domain.size(); d++) {
                 displ[d] = 9 * d;
             }
-            MPI_Allgatherv(local_stress.data(), 9, MPI_DOUBLE, recv.data(), recvcount.data(), displ.data(), MPI_DOUBLE, MPI_COMM_WORLD);
+            MPI_Allgatherv(local_stress.data(), 9, MPI_DOUBLE, recv.data(),
+                           recvcount.data(), displ.data(), MPI_DOUBLE,
+                           MPI_COMM_WORLD);
             Eigen::Matrix3d stress;
             stress.setZero();
             if (domain.rank() == 0) {
-//                                std::cout << recv << std::endl;
+                //                                std::cout << recv <<
+                //                                std::endl;
                 for (int d = 0; d < domain.size(); d++) {
-                    stress += recv.block<3,3>(0,d*3);
+                    stress += recv.block<3, 3>(0, d * 3);
                 }
-//                std::cout << stress(2,2) << std::endl;
+                //                std::cout << stress(2,2) << std::endl;
             }
-//            MPI_Comm comm = domain.communicator();
-            //            MPI::Eigen::allgather(stress, recv, comm); // This part does not work
+            //            MPI_Comm comm = domain.communicator();
+            //            MPI::Eigen::allgather(stress, recv, comm); // This
+            //            part does not work
             domain.disable(atoms);
             if (traj != nullptr) {
                 assert(rank == 0);
                 write_xyz(*traj, atoms);
                 // Slower cheaty way of only measuring potentials of non-ghost
                 // atoms for write operations
-//                neighborList.update(atoms, cutoff);
-//                double pot = ducastelle(atoms, neighborList, cutoff);
+                //                neighborList.update(atoms, cutoff);
+                //                double pot = ducastelle(atoms, neighborList,
+                //                cutoff);
                 auto kinetic = kinetic_energy(atoms);
                 std::cout << i << "," << (i)*timestep << "," << pot + kinetic
                           << "," << pot << "," << kinetic << ","
                           << temperature(atoms) << ","
                           << (len[2] - original_length) / original_length << ","
-                          << stress(2,2) << std::endl;
+                          << stress(2, 2) << std::endl;
             }
             domain.enable(atoms);
         }
