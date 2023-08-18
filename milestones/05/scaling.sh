08e538dafe22e148c725eb80022b18d814329a08
Joshua Faessler <jffaessler@gmail.com>
2023-08-18 20:43:33 +0200

    forgot to time the ting

--- milestones/05/scaling.sh
+++ milestones/05/scaling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./05 4 > size_l4.csv
-./05 5 > size_l5.csv
-./05 6 > size_l6.csv
-./05 7 > size_l7.csv
-./05 8 > size_l8.csv
-./05 9 > size_l9.csv
+time ./05 4 > size_l4.csv
+time ./05 5 > size_l5.csv
+time ./05 6 > size_l6.csv
+time ./05 7 > size_l7.csv
+time ./05 8 > size_l8.csv
+time ./05 9 > size_l9.csv
