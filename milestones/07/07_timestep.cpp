fa5cc1490e8912f2abf35a0950b58a775a4197ba
Joshua Faessler <jffaessler@gmail.com>
2023-08-30 14:00:51 -0500

    Determination of timestep

--- milestones/07/07_timestep.cpp
+++ milestones/07/07_timestep.cpp
@@ -6,13 +6,21 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    double timestep = std::stod(argv[1]);
-    int steps = static_cast<int>(1. / timestep / 1.);
+    double timestep;
+    if (argc > 1) {
+        timestep = std::stod(argv[1]);
+    } else {
+        timestep = 1.;
+    }
+    int steps = static_cast<int>(500. / timestep);
     std::ofstream traj("traj.xyz");
     std::ofstream ts(std::to_string(timestep) + ".timestep");
 
     auto [names, init_positions]{read_xyz("cluster_923.xyz")};
-    Atoms atoms(init_positions);
+    double gold_mass =
+        20413.15887; // Gold in system's mass units where [m] = 0.009649 g/mol
+    Atoms atoms(init_positions, gold_mass);
+    atoms.k_b = 8.617333262e-5; // Boltzmann constant in eV/K
 
     const double cutoff = 5.0;
     NeighborList neighborList;
@@ -22,12 +30,13 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Time,Total Energy,Potential,Kinetic,Temperature" << std::endl;
     std::cout.precision(10);
+    ts << "Time,Total Energy" << std::endl;
     ts.precision(10);
     for (int i = 0; i < steps; ++i) {
-        verlet_step1(atoms.positions, atoms.velocities, atoms.forces, timestep);
+        verlet_step1(atoms.positions, atoms.velocities, atoms.forces, timestep, atoms.mass);
         neighborList.update(atoms, cutoff);
         double pot = ducastelle(atoms, neighborList, cutoff);
-        verlet_step2(atoms.velocities, atoms.forces, timestep);
+        verlet_step2(atoms.velocities, atoms.forces, timestep, atoms.mass);
 
         write_xyz(traj, atoms);
         auto kinetic = Eigen::pow(atoms.velocities, 2).sum() / 2;
---
+++ milestones/07/timesteps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./07_timestep .1
+./07_timestep 1.
+./07_timestep 10.
+./07_timestep 15.
+./07_timestep 20.
+./07_timestep 50.
+./07_timestep 100.
